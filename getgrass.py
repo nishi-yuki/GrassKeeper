53a0ffaf938deb495684f12069bd2608b51f53c6
Nishi Yuki <nishiyk256@gmail.com>
2022-10-13 13:20:12 +0900

    refactoring

--- getgrass.py
+++ getgrass.py
@@ -3,54 +3,67 @@ import os
 import json
 import requests
 
-ENDPOINT = 'https://api.github.com/graphql'
+
 TZ_INFO = timezone(timedelta(hours=9))
 
-with open('.env', 'rt') as f:
-    for l in f.readlines():
-        key, value = l.split('=')
-        value = value.strip()
-        value = value.strip('\'"')
-        os.environ[key] = value
 
-token = os.environ['GITHUB_TOKEN']
-username = os.environ['GITHUB_USERNAME']
+def read_env():
+    with open('.env', 'rt') as f:
+        for l in f.readlines():
+            key, value = l.split('=')
+            value = value.strip()
+            value = value.strip('\'"')
+            os.environ[key] = value
 
-headers = {'Authorization': 'bearer ' + token}
 
-query = '''
-query($userName:String!, $from:DateTime!, $to:DateTime!) {
-  user(login: $userName){
-    contributionsCollection (from: $from, to: $to) {
-      contributionCalendar {
-        weeks {
-          contributionDays {
-            contributionCount
-            date
+class GithubApiClient:
+    endpoint = 'https://api.github.com/graphql'
+    query = '''
+    query($userName:String!, $from:DateTime!, $to:DateTime!) {
+      user(login: $userName){
+        contributionsCollection (from: $from, to: $to) {
+          contributionCalendar {
+            weeks {
+              contributionDays {
+                contributionCount
+                date
+              }
+            }
           }
         }
       }
     }
-  }
-}
-'''
+    '''
+
+    def __init__(self, token, username):
+        self.headers = {'Authorization': 'bearer ' + token}
+        self.username = username
+
+    def fetch_grass_info(self, fromdate, todate):
+        variables = {'userName': username, 'from': fromdate, 'to': todate}
+        variables = json.dumps(variables)
+        payload = {'query': self.query, 'variables': variables}
+
+        res = requests.post(self.endpoint, headers=self.headers,
+                            data=json.dumps(payload))
+        res = res.json()
+        print(json.dumps(res, indent=2))
 
 
-today = date.today()
-today_start = datetime.combine(today, time.min, tzinfo=TZ_INFO)
-today_start = today_start.isoformat()
-today_end = datetime.combine(today, time.max, tzinfo=TZ_INFO)
-today_end = today_end.isoformat()
-
-variables = {'userName': username, 'from': today_start, 'to': today_end}
-variables = json.dumps(variables)
-
-print(variables)
+def calc_today_start_and_end():
+    today = date.today()
+    today_start = datetime.combine(today, time.min, tzinfo=TZ_INFO)
+    today_start = today_start.isoformat()
+    today_end = datetime.combine(today, time.max, tzinfo=TZ_INFO)
+    today_end = today_end.isoformat()
+    return today_start, today_end
 
 
-payload = {'query': query, 'variables': variables}
+if __name__ == '__main__':
+    read_env()
+    token = os.environ['GITHUB_TOKEN']
+    username = os.environ['GITHUB_USERNAME']
+    start, end = calc_today_start_and_end()
 
-res = requests.post(ENDPOINT, headers=headers, data=json.dumps(payload))
-res = res.json()
-
-print(json.dumps(res, indent=2))
+    client = GithubApiClient(token, username)    
+    client.fetch_grass_info(start, end)
